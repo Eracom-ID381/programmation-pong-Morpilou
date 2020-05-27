31c7a0b5b24dcffef7af97e8767e417297cb74f0
Morpilou <Morpilou@users.noreply.github.com>
2020-05-27 16:54:59 +0200

    creat a class for the paddles

--- js/sketch.js
+++ js/sketch.js
@@ -2,115 +2,120 @@ let scoreLeft = 0;
 let scoreRight = 0;
 
 let ball = {
-    x: 0,
-    y: 0,
-    speedX: 10,
-    speedY: 0,
-    radius: 40
+  x: 0,
+  y: 0,
+  speedX: 10,
+  speedY: 0,
+  radius: 40
 }
 
-let paddleLeft = {
-    x: 30,
-    y: 0,
-    width: 20,
-    height: 150
-}
+let paddleLeft;
+let paddleRight;
 
-let paddleRight = {
-    x: 0,
-    y: 0,
-    width: 20,
-    height: 150
+class Paddle {
+  constructor(_x, _y, _width, _height) {
+    this.x = _x;
+    this.y = _y;
+    this.width = _width;
+    this.height = _height;
+
+  }
+
+  show() {
+    rect(this.x, this.y, this.width, this.height);
+  }
+
+  slide(mouseAxis) {
+    this.y = mouseAxis;
+  }
 }
 
 
 function setup() {
-    createCanvas(window.innerWidth, window.innerHeight);
-    rectMode(CENTER);
-    noStroke();
+  createCanvas(window.innerWidth, window.innerHeight);
+  rectMode(CENTER);
+  noStroke();
 
-    ball.x = width / 2;
-    ball.y = height / 2;
+  paddleLeft = new Paddle(30, 0, 20, 150);
+  paddleRight = new Paddle(width - 30, 0, 20, 150);
 
-    paddleRight.x = width - 30;
+  ball.x = width / 2;
+  ball.y = height / 2;
+
 }
 
 function draw() {
-    background(0);
-    fill(255);
-    moveBall();
-    bounceBall();
-    drawElements();
+  background(0);
+  fill(255);
+  moveBall();
+  bounceBall();
+  paddleLeft.show();
+  paddleLeft.slide(mouseX);
+  paddleRight.show();
+  paddleRight.slide(mouseY);
+  drawElements();
 }
 
 function drawElements() {
-    paddleRight.y = mouseY;
-    paddleLeft.y = mouseX;
-    rect(paddleLeft.x, paddleLeft.y, paddleLeft.width, paddleLeft.height);
-    rect(paddleRight.x, paddleRight.y, paddleRight.width, paddleRight.height);
-    ellipse(ball.x, ball.y, ball.radius);
-    textSize(100);
-    textAlign(RIGHT)
-    text(scoreLeft, width / 2 - 40, 100);
-    textAlign(LEFT)
 
-    text(scoreRight, width/2 + 40 , 100);
+  ellipse(ball.x, ball.y, ball.radius);
+  textSize(100);
+  textAlign(RIGHT)
+  text(scoreLeft, width / 2 - 40, 100);
+  textAlign(LEFT)
 
-    for (let y = 0; y < height; y = y + 30) {
-        rect(width / 2, y, 20, 20);
-    }
+  text(scoreRight, width / 2 + 40, 100);
+
+  for (let y = 0; y < height; y = y + 30) {
+    rect(width / 2, y, 20, 20);
+  }
 }
 
 function bounceBall() {
-    // Detection de collision Paddle Right
-    if (ball.x >= paddleRight.x - paddleRight.width * 2 &&
-        ball.y >= paddleRight.y - paddleRight.height / 2 &&
-        ball.y <= paddleRight.y + paddleRight.height / 2) {
-        ball.speedX = -ball.speedX;
-        ball.speedY = random(-5, 5);
-    }
+  // Detection de collision Paddle Right
+  if (ball.x >= paddleRight.x - paddleRight.width * 2 &&
+    ball.y >= paddleRight.y - paddleRight.height / 2 &&
+    ball.y <= paddleRight.y + paddleRight.height / 2) {
+    ball.speedX = -ball.speedX;
+    ball.speedY = random(-5, 5);
+  }
 
-    // Detection de collision Paddle Left 
-    if (ball.x <= paddleLeft.x + paddleLeft.width * 2 &&
-        ball.y >= paddleLeft.y - paddleLeft.height / 2 &&
-        ball.y <= paddleLeft.y + paddleLeft.height / 2) {
-        ball.speedX = -ball.speedX;
-        ball.speedY = random(-5, 5);
-    }
+  // Detection de collision Paddle Left
+  if (ball.x <= paddleLeft.x + paddleLeft.width * 2 &&
+    ball.y >= paddleLeft.y - paddleLeft.height / 2 &&
+    ball.y <= paddleLeft.y + paddleLeft.height / 2) {
+    ball.speedX = -ball.speedX;
+    ball.speedY = random(-5, 5);
+  }
 
-    // Detection collision "murs" haut et bas
-    if (ball.y <= ball.radius || ball.y >= height - ball.radius) {
-        ball.speedY = -ball.speedY;
-    }
+  // Detection collision "murs" haut et bas
+  if (ball.y <= ball.radius || ball.y >= height - ball.radius) {
+    ball.speedY = -ball.speedY;
+  }
 
-    if (ball.x > width) {
-        resetBall('left');
-        scoreLeft += 1;
-    } else if (ball.x < 0) {
-        resetBall('right');
-        scoreRight += 1;
-    }
-}
-
-function movePaddle() {
-    paddleRight.y = mouseY;
-    paddleLeft.y = mouseX;
+  if (ball.x > width) {
+    resetBall('left');
+    scoreLeft += 1;
+  } else if (ball.x < 0) {
+    resetBall('right');
+    scoreRight += 1;
+  }
 }
 
 function moveBall() {
-    ball.x += ball.speedX;
-    ball.y += ball.speedY;
+  ball.x += ball.speedX;
+  ball.y += ball.speedY;
 }
 
 function resetBall() {
-    ball.x = width / 2;
-    ball.y = height / 2;
-    ball.speedX = -ball.speedX;
-    ball.speedY = random(-2, 2);
+  ball.x = width / 2;
+  ball.y = height / 2;
+  ball.speedX = -ball.speedX;
+  ball.speedY = random(-2, 2);
 
 }
 
 function windowResized() {
-    resizeCanvas(window.innerWidth, window.innerHeight);
-    setup();
+  resizeCanvas(window.innerWidth, window.innerHeight);
+  setup();
 }
